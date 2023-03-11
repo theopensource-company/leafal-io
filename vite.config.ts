3c57c9a6861894059b19a8db3cf77260031a777b
Micha de Vries <micha@devrie.sh>
2023-03-11 12:27:56 +0100

    Deploy to cloudflare pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
+import cloudflare from 'solid-start-cloudflare-pages';
 import solid from 'solid-start/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    plugins: [solid()],
+    plugins: [
+        solid({
+            adapter: cloudflare({}),
+        }),
+    ],
 });
