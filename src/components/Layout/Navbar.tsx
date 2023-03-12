15abe18c6068088d657d9b074f98473cb2157751
Morgan Hofmann <morgan@theopensource.company>
2023-03-12 14:05:12 +0100

    Hover/tab animations

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -1,11 +1,16 @@
 import { cva } from 'cva';
-import { JSX } from 'solid-js';
+import { ComponentProps, JSX, splitProps } from 'solid-js';
+import { A } from 'solid-start';
 import style from '~/styles/components/Layout/Navbar.module.scss';
 import { Logo } from '../Brand/Logo';
 import { Button } from '../Button';
-import { NavItem } from './NavItem';
 
 export const navbarStyle = cva([style.default]);
+
+export function NavItem(_props: ComponentProps<typeof A>) {
+    const [, rest] = splitProps(_props, []);
+    return <A class={style.item} {...rest} />;
+}
 
 export type NavbarProps = object;
 
@@ -17,9 +22,11 @@ export function Navbar(props: JSX.HTMLElementTags['div'] & NavbarProps) {
             })}
         >
             <div class={style.content}>
-                <NavItem href="/" class={style.logo}>
-                    <Logo />
-                    <span>leafal.io</span>
+                <NavItem href="/" tabIndex="0">
+                    <div class={style.logo}>
+                        <Logo />
+                        <span>leafal.io</span>
+                    </div>
                 </NavItem>
                 <div class={style.links} />
                 <div class={style.account}>
