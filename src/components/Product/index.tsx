d8a72938c48157c93312f592224b1f69ad49e412
Morgan Hofmann <morgan@morganhofmann.nl>
2023-03-12 01:14:40 +0100

    Proper descriptions for Celesteia as a mockup

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -3,10 +3,9 @@ import { MockupProduct } from 'constants/Types/Products.types';
 export function mockup(slug: string): MockupProduct {
     return {
         slug: slug,
-        title: slug.substring(0, 1).toUpperCase() + slug.substring(1),
-        tagline: 'This is a mockup product.',
-        overview:
-            'This is a mockup of a product intended for testing purposes, it is not a real product and you cannot buy or play it.',
+        title: 'Celesteia',
+        tagline: `Humankind's last hope spreads across the galaxy to rebuild humanity's glory.`,
+        overview: `In the wake of a terrible calamity, humankind's last hope spreads across the galaxy to rebuild humanity's glory.`,
         description:
             'This is a mockup of a product intended for testing purposes, it is not a real product and you cannot buy or play it. Please, do not believe anyone that tells you you can buy this. You cannot. Believe me.',
         logo: `https://raw.githubusercontent.com/leafal-io/${slug}/production/Icon.bmp`,
