2cdf5d9f6ddf0590b98bb0310440d24ab3169cf3
Morgan Hofmann <morgan@morganhofmann.nl>
2023-03-12 03:18:39 +0100

    Fix title on navigation back to /

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,13 @@
+import { Title } from 'solid-start';
 import { mockup } from '~/components/Product';
 import { ProductSummary } from '~/components/Product/Summary';
 
 export default function Home() {
-    return <ProductSummary product={mockup('celesteia')} />;
+    const standardTitle = 'leafal.io';
+    return (
+        <>
+            <Title>{standardTitle}</Title>
+            <ProductSummary product={mockup('celesteia')} />
+        </>
+    );
 }
