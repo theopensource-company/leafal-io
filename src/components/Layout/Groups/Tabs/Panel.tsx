2dec52f20b70b8315ac204c4e962dba4dcf76497
Micha de Vries <micha@devrie.sh>
2023-03-19 11:58:02 +0100

    Improve default panel styling

--- src/components/Layout/Groups/Tabs/Panel.tsx
+++ src/components/Layout/Groups/Tabs/Panel.tsx
@@ -1,12 +1,19 @@
+import { cva } from 'cva';
 import { createSignal, JSX, splitProps, useContext } from 'solid-js';
+import styles from '~/styles/components/Layout/Groups/Tabs.module.scss';
 import { TabsContext } from '.';
 
 export function TabPanel(_props: JSX.HTMLElementTags['div']) {
     const tabsContext = useContext(TabsContext);
     const [index, setIndex] = createSignal(-1);
-    const [split, rest] = splitProps(_props, ['ref']);
+    const [split, rest] = splitProps(_props, ['ref', 'class']);
 
     const active = () => index() == tabsContext?.activeIndex();
+    const classes = () =>
+        tabPanelStyles({
+            state: active() ? 'active' : 'idle',
+            class: split.class,
+        });
 
     return (
         <div
@@ -20,10 +27,20 @@ export function TabPanel(_props: JSX.HTMLElementTags['div']) {
                     split.ref = el;
                 }
             }}
-            style={{
-                display: active() ? undefined : 'none',
-            }}
+            class={classes()}
             {...rest}
         />
     );
 }
+
+export const tabPanelStyles = cva([styles.tabPanel], {
+    variants: {
+        state: {
+            active: [styles.tabPanelActive],
+            idle: [styles.tabPanelIdle],
+        },
+    },
+    defaultVariants: {
+        state: 'idle',
+    },
+});
