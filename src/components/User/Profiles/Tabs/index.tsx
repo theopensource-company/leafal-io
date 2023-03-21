99b5ffaed2ea08a0af76b14e3e2c5527235747d9
Morgan Hofmann <morgan@morganhofmann.nl>
2023-03-21 19:30:49 +0100

    Final profile layout

--- src/components/User/Profiles/Tabs/index.tsx
+++ src/components/User/Profiles/Tabs/index.tsx
@@ -9,23 +9,6 @@ import styles from '~/styles/components/User/Profiles/Tabs.module.scss';
 
 export default function ProfileTabs() {
     return (
-        <Tab.Group direction="vertical">
-            <Tab.List class={styles.list}>
-                <Overview.Tab />
-                <Friends.Tab />
-                <GameList.Tab />
-            </Tab.List>
-
-            <ColumnBar>
-                <Column variant="twothird">
-                    <Overview />
-                    <Friends />
-                    <GameList />
-                </Column>
-                <Column variant="onethird">
-                    <></>
-                </Column>
-            </ColumnBar>
-        </Tab.Group>
+        
     );
 }
