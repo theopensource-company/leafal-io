32937cda66bc80421f26e29b9b44d7661c6b7756
Morgan Hofmann <morgan@morganhofmann.nl>
2023-03-24 23:54:33 +0100

    Remove example profile cards from homepage

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,27 +2,15 @@ import { Title } from 'solid-start';
 import { MainWrapper } from '~/components/Layout/MainWrapper';
 import { mockupProduct } from '~/components/Product';
 import { ProductSummary } from '~/components/Product/Summary';
-import { mockupUser, toPublic } from '~/components/User';
-import { ProfileCard } from '~/components/User/Profiles/Card';
 
 export default function Home() {
     const standardTitle = 'leafal.io';
+
     return (
         <>
             <Title>{standardTitle}</Title>
             <ProductSummary product={mockupProduct('celesteia')} />
-            <MainWrapper>
-                <ProfileCard
-                    size="large"
-                    user={toPublic(mockupUser('johndoe'))}
-                    status="online"
-                />
-                <ProfileCard
-                    size="large"
-                    user={toPublic(mockupUser('johndoe'))}
-                    status="online"
-                />
-            </MainWrapper>
+            <MainWrapper />
         </>
     );
 }
