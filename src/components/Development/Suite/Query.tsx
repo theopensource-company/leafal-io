c2cd79a3bc5c06006388ec1f33d9d4d5d3368690
Micha de Vries <micha@devrie.sh>
2023-03-27 12:53:40 +0200

    Run on client only :)

--- src/components/Development/Suite/Query.tsx
+++ src/components/Development/Suite/Query.tsx
@@ -1,40 +1,47 @@
 import { createSignal } from 'solid-js';
+import { unstable_clientOnly } from 'solid-start';
 import { Button } from '~/components/Button';
 import { Tab } from '~/components/Layout/Groups/Tabs';
-import { SurrealInstance } from '~/library/Surreal';
 import styles from '~/styles/components/Development/Suite.module.scss';
 
-export function Query() {
-    const [input, setInput] = createSignal('');
-    const [output, setOutput] = createSignal('Run a query for some output');
+export const Query = unstable_clientOnly(async () => {
+    const { SurrealInstance } = await import('~/library/Surreal');
+    return {
+        default: () => {
+            const [input, setInput] = createSignal('');
+            const [output, setOutput] = createSignal(
+                'Run a query for some output'
+            );
 
-    const submit = async () => {
-        if (input().length > 0) {
-            SurrealInstance.opiniatedQuery(input()).then((res) => {
-                setOutput(JSON.stringify(res, null, 2));
-            });
-        } else {
-            alert('No query');
-        }
+            const submit = () => {
+                if (input().length > 0) {
+                    SurrealInstance.opiniatedQuery(input()).then((res) => {
+                        setOutput(JSON.stringify(res, null, 2));
+                    });
+                } else {
+                    alert('No query');
+                }
+            };
+
+            return (
+                <Tab.Panel>
+                    <div class={styles.queryPanel}>
+                        <h1>Query the database</h1>
+
+                        <textarea
+                            placeholder="Query"
+                            value={input()}
+                            onInput={(e) => setInput(e.currentTarget.value)}
+                        />
+                        <Button onClick={submit}>Execute</Button>
+                        <div>{output()}</div>
+                    </div>
+                </Tab.Panel>
+            );
+        },
     };
+});
 
-    return (
-        <Tab.Panel>
-            <div class={styles.queryPanel}>
-                <h1>Query the database</h1>
-
-                <textarea
-                    placeholder="Query"
-                    value={input()}
-                    onInput={(e) => setInput(e.currentTarget.value)}
-                />
-                <Button onClick={submit}>Execute</Button>
-                <div>{output()}</div>
-            </div>
-        </Tab.Panel>
-    );
-}
-
-Query.Tab = function () {
+export const QueryTab = function () {
     return <Tab default>Query</Tab>;
 };
