230d23cbafbbc36e0dd5402a371aeb1bd9227fca
Micha de Vries <micha@devrie.sh>
2023-03-30 20:00:57 +0200

    Got it working!

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
-import cloudflare from 'solid-start-cloudflare-pages';
+import devTools from 'solid-devtools/vite';
+import deno from 'solid-start-deno';
 import solid from 'solid-start/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
     plugins: [
+        devTools(),
         solid({
-            adapter: cloudflare({}),
+            adapter: deno(),
         }),
     ],
 });
