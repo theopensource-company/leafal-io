48eec89d947154a21ac3a4224551e590bf28c14c
Leafeling <morgan@morganhofmann.nl>
2023-05-21 15:16:03 +0200

    Product previews

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,10 @@ export const metadata: Metadata = {
     title: 'leafal.io',
 };
 
+export function MainWrapper({ children }: { children: React.ReactNode }) {
+  return <div className="main-wrapper">{children}</div>;
+}
+
 export default function RootLayout({
     children,
 }: {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,32 @@
 
 import * as React from 'react';
 
-import SignInForm from './components/Forms/SignInForm';
-import SignUpForm from './components/Forms/SignUpForm';
+import SignInForm from "./components/Forms/SignInForm";
+import SignUpForm from "./components/Forms/SignUpForm";
+import { useEffect, useState } from "react";
+import { SurrealInstance } from "./lib/Surreal";
+import { TProductRecord } from "@/constants/types/Product.types";
+import { ProductPreviewBanner } from "./components/Product/Previews";
 
 export default function Home() {
-    return (
-        <main>
-            <SignInForm />
-            <SignUpForm />
-        </main>
-    );
+  const [product, setProduct] = useState<TProductRecord>();
+
+  useEffect(() => {
+    async function fetchProduct() {
+      const result = await SurrealInstance.query<[TProductRecord[]]>(
+        "SELECT * FROM product"
+      );
+
+      if (result?.[0]?.result?.[0]) setProduct(result[0].result[0]);
+    }
+    fetchProduct();
+  }, []);
+
+  return (
+    <main>
+      {product && <ProductPreviewBanner product={product} />}
+      <SignInForm />
+      <SignUpForm />
+    </main>
+  );
 }
