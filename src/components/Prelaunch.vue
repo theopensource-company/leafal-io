7b463ea1998fbec23067379e623b120ec3da54c1
Hazel Hofmann <hazel@theopensource.company>
2023-10-13 15:30:55 +0200

    Prelaunch page pt 2

---
+++ src/components/Prelaunch.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="base">
+    <img class="logo" src="/logo.svg">
+  </div>
+</template>
+
+<style scoped lang="scss">
+  .base {
+    margin: 0 auto;
+    display: grid;
+    place-items: center;
+    width: 100%;
+    min-height: 100vh;
+    min-height: 100svh;
+
+    background: radial-gradient(circle at 50%, var(--dark2), var(--dark));
+  }
+
+  .logo {
+    max-width: 10em;
+    filter: none;
+    transition: filter .2s ease;
+    &:hover { filter: drop-shadow(0 0 .5em #3dad2caa); }
+  }
+</style>
