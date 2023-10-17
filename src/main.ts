b95e00c3c67ef49c6a083a4636bbe1d67812868a
Hazel Hofmann <hazel@theopensource.company>
2023-10-17 11:15:57 +0200

    Lazy loading is actually not needed

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,12 @@ import './style.css';
 import App from './App.vue';
 import { createMetaManager } from 'vue-meta';
 import { createRouter, createWebHistory } from 'vue-router';
+import HomeVue from './routes/Home.vue';
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        { path: '/', component: () => import('@/routes/Home.vue') }
+        { path: '/', component: HomeVue }
     ]
 })
 
