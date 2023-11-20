1d8781c771b1d1607ceea6f179f5bf7d724057a1
Hazel Hofmann <hazel@theopensource.company>
2023-11-20 12:56:29 +0100

    Add product queries

---
+++ src/library/queries/Product.queries.ts
@@ -0,0 +1,10 @@
+import { Product } from '@/constants/types/Product.types';
+import { database } from '../surreal';
+
+export const getAllProducts = async () => {
+    const queries = await database.query<[Product[]]>('SELECT * FROM product');
+    const products = queries[0];
+
+    products.map((p) => Product.parse(p));
+    return products;
+};
