8522d313dcd20c8e1fc1c9b4297fe87d952858cd
Hazel Hofmann <hazel@theopensource.company>
2023-12-03 15:33:21 +0100

    Use SurrealDB variables from env

--- composables/useDatabase.ts
+++ composables/useDatabase.ts
@@ -2,9 +2,9 @@ import { Surreal } from "surrealdb.js"
 
 export const useDatabase = async () => {
     const database = new Surreal()
-    await database.connect('http://localhost:14001', {
-        namespace: 'leafal-io',
-        database: 'leafal-io-deployment_development'
+    await database.connect(import.meta.env.VITE_SURREAL_ENDPOINT, {
+        namespace: import.meta.env.VITE_SURREAL_NAMESPACE ?? 'leafal-io',
+        database: import.meta.env.VITE_SURREAL_DATABASE ?? 'leafal-io-deployment_local',
     })
     return database
 }
